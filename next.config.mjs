0cfd4bd46d91475a3f790e16d5c089116566b577
= <=>
2024-03-15 22:05:55 +0530

    some changes on next.config file  for better loading of images

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  output: "standalone",
   images: {
-    domains: ["localhost", "diva-backend-iukkr.ondigitalocean.app", "diva-images.blr1.digitaloceanspaces.com"],
+    domains: [
+      "localhost",
+      "diva-backend-iukkr.ondigitalocean.app",
+      "diva-images.blr1.digitaloceanspaces.com",
+    ],
   },
 };
-
 
 export default nextConfig;
