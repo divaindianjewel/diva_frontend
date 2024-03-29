1fb94257a1ed819c44f845edb54f8f7cc2b67bea
= <=>
2024-03-29 12:37:00 +0530

    init the form

---
+++ app/payment/page.tsx
@@ -0,0 +1,12 @@
+import PaymentForm from '@/backend/phonepe/payment-form'
+import React from 'react'
+
+const Payment = () => {
+  return (
+    <div>
+        <PaymentForm />
+    </div>
+  )
+}
+
+export default Payment
