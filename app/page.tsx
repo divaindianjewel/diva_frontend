583cbf1969931c1669a6e1c864b500e79e40a6eb
= <=>
2024-04-02 20:29:43 +0530

    removing some unwanted import

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import MensFeatureSection from "@/components/custom/swiper/MensFeatureSection";
 import WomensFeatureSection from "@/components/custom/swiper/WomensFeatureSection";
 import FeaturesSection from "@/components/custom/swiper/FeatureSection";
 import Categories from "@/components/custom/swiper/categoriesSection";
-import TmpSection from "@/components/custom/swiper/tmp-section";
 
 // Images
 export default async function Home() {
