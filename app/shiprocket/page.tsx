fc28b6497c94a8670862e9078dd489cc79e34e2c
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-04-24 23:54:54 +0530

    adding loading effect to the shipping page

--- app/shiprocket/page.tsx
+++ app/shiprocket/page.tsx
@@ -105,7 +105,7 @@ const Page = () => {
       // console.log("CartData : " + cartData.length);
       // console.log("total : " + total);
       // console.log("UserId : " + userId);
-      // console.log("UserName : " + userName); 
+      // console.log("UserName : " + userName);
       // console.log("___________________________");
       if (userName != undefined && total != 0) {
         console.log("CartData : " + cartData.length);
@@ -132,7 +132,35 @@ const Page = () => {
     setTotal(totalPrice);
   }, [cartData]);
 
-  return <div>working</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <svg
+          className="animate-spin h-8 w-8 text-gray-600"
+          fill="none"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          />
+          <path
+            className="opacity-75"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            fill="currentColor"
+          />
+        </svg>
+        <p className="mt-4 text-gray-500 dark:text-gray-400">
+          please wait your order is placing
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
