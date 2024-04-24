2af090ec2a28b653d235301fab65f7c070bfa69d
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-04-24 15:06:28 +0530

    some changes

--- backend/order/create-orderId.tsx
+++ backend/order/create-orderId.tsx
@@ -12,27 +12,34 @@ const CreateOrderId = async (
   var month = currentDate.getMonth() + 1; // Note: January is 0
   var year = currentDate.getFullYear();
 
-  try {
-    const response = await fetch(`${domain}/api/orders`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({
-        data: {
-          user_id: user_id,
-          order_date: `${day}-${month}-${year}`,
-          total_price: price,
-          discount: discount,
-          user_name: user_name,
+  if (
+    user_id != null &&
+    user_id != undefined &&
+    user_name != null &&
+    user_name != undefined
+  ) {
+    try {
+      const response = await fetch(`${domain}/api/orders`, {
+        headers: {
+          "Content-Type": "application/json",
         },
-      }),
-    });
-    const data = await response.json();
-    return data.data;
-  } catch (error) {
-    console.log(error);
-    return 0;
+        method: "POST",
+        body: JSON.stringify({
+          data: {
+            user_id: user_id,
+            order_date: `${day}-${month}-${year}`,
+            total_price: price,
+            discount: discount,
+            user_name: user_name,
+          },
+        }),
+      });
+      const data = await response.json();
+      return data.data;
+    } catch (error) {
+      console.log(error);
+      return 0;
+    }
   }
 };
 
