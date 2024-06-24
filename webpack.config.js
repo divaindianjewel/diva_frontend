e46deb065025ebc10c715ad2406d9702c5ffe197
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-06-24 13:55:06 +0530

    some changes

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// webpack.config.js
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
